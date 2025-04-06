b5c20a1e2895313b9247482dcc58e4c145b26ef8
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2025-04-06 20:08:15 -0300

    Executes commands and prints their output

--- main.go
+++ main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 )
 
+// Executes a command and prints the output.
+func runNative() error {
+	cmd := exec.Command("./game.sh")
+	stdout, err := cmd.StdoutPipe()
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = cmd.Stdout
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	// https://stackoverflow.com/a/62630988
+	// Hmmm...
+	for {
+		tmp := make([]byte, 1)
+		_, err := stdout.Read(tmp)
+		fmt.Print(string(tmp))
+		if err != nil {
+			break
+		}
+	}
+
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	// Get executable path from somewhere
+	// Then pass it to runNative()
+	runNative()
 }
