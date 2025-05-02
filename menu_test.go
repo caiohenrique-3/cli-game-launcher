e2a615500f434f99c56067f47779ffdff0182822
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2025-05-02 11:52:04 -0300

    Fix tests using actual program files instead of temp ones

--- menu_test.go
+++ menu_test.go
@@ -31,7 +31,7 @@ func TestAddGamePromptFileNotFound(t *testing.T) {
 	t.Fatalf("process ran with err %v; want exit status 1", err)
 }
 
-func TestAddGameExecutableGoodEnding(t *testing.T) {
+func TestAddGamePromptGoodEnding(t *testing.T) {
 	f, err := createTempFileOnOsTempDir()
 	if err != nil {
 		t.Error(err)
@@ -39,10 +39,14 @@ func TestAddGameExecutableGoodEnding(t *testing.T) {
 
 	input := "Test Game Name\n" + f.Name() + "\n"
 	r := bufio.NewReader(strings.NewReader(input))
+
+	orig := programHome
+	programHome = os.TempDir()
 	addGamePrompt(r)
+	programHome = orig
 }
 
-func TestAddGameExecutablePathWithSpaces(t *testing.T) {
+func TestAddGamePromptPathWithSpaces(t *testing.T) {
 	testDir, err := os.MkdirTemp("", "Dir With Spaces")
 	if err != nil {
 		t.Error(err)
@@ -56,10 +60,13 @@ func TestAddGameExecutablePathWithSpaces(t *testing.T) {
 
 	input := "Test Game Name\n" + f.Name() + "\n"
 	r := bufio.NewReader(strings.NewReader(input))
+	orig := programHome
+	programHome = os.TempDir()
 	addGamePrompt(r)
+	programHome = orig
 }
 
-func TestAddGameExecutablePathIsADirectory(t *testing.T) {
+func TestAddGamePromptPathIsADirectory(t *testing.T) {
 	if os.Getenv("BE_CRASHER") == "1" {
 		testDir, err := os.MkdirTemp("", "TESTDIR")
 		if err != nil {
@@ -72,7 +79,7 @@ func TestAddGameExecutablePathIsADirectory(t *testing.T) {
 		return
 	}
 	cmd := exec.Command(os.Args[0],
-		"-test.run=TestAddGameExecutablePathIsADirectory")
+		"-test.run=TestAddGamePromptPathIsADirectory")
 	cmd.Env = append(os.Environ(), "BE_CRASHER=1")
 	err := cmd.Run()
 	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
@@ -81,7 +88,7 @@ func TestAddGameExecutablePathIsADirectory(t *testing.T) {
 	t.Fatalf("process ran with err %v; want exit status 1", err)
 }
 
-func TestAddGameExecutableExpandsEnvVariables(t *testing.T) {
+func TestAddGamePromptExpandsEnvVariables(t *testing.T) {
 	if os.Getenv("TEST_TMPDIR") != "" {
 		f, err := createTempFileOnOsTempDir()
 		if err != nil {
@@ -93,12 +100,15 @@ func TestAddGameExecutableExpandsEnvVariables(t *testing.T) {
 		input := "Test Game Name\n" +
 			"$TEST_TMPDIR/" + fileName + "\n"
 		r := bufio.NewReader(strings.NewReader(input))
+		orig := programHome
+		programHome = os.TempDir()
 		addGamePrompt(r)
+		programHome = orig
 		return
 	}
 
 	cmd := exec.Command(os.Args[0],
-		"-test.run=TestAddGameExecutableExpandsEnvVariables")
+		"-test.run=TestAddGamePromptExpandsEnvVariables")
 	cmd.Env = append(os.Environ(), "TEST_TMPDIR="+os.TempDir())
 	err := cmd.Run()
 	if err != nil {
